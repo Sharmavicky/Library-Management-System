9d142034dedc11ab0e782229166c78c3dde9b45c
Sharmavicky <sharmavicky93sh@gmail.com>
2026-05-31 19:07:01 +0530

    fix: simplify database connection logic and improve error logging

--- Backend/src/config/dbConnection.js
+++ Backend/src/config/dbConnection.js
@@ -1,15 +1,12 @@
 const mongoose = require("mongoose");
 
-// function to connect to the database
 const dbConnection = async () => {
     try {
-        mongoose.connect(process.env.MONGO_URI).then(() => {
-            console.log("Connected to DB:", mongoose.connection.name); // logs DB name
-        });
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Connected to DB:", mongoose.connection.name);
     } catch (err) {
-        console.error("Error connecting to database: ", err);
+        console.error("Error connecting to database:", err);
     }
-}
+};
 
-// export the db connection function
 module.exports = dbConnection;
